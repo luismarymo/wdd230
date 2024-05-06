4148f6863608ce34d3d0d53c25e50b395b0c81d1
luismarymo <luchita0420@gmail.com>
2024-05-06 13:57:40 +0200

    Lazy Loading Activity

---
+++ week03/scripts/main.js
@@ -0,0 +1,5 @@
+//date of last modification
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
+//copyright year
+document.querySelector('#year').textContent = new Date().getFullYear();
