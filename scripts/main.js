20f59c63e9ca2369080884299cc0ca7a1044827a
luismarymo <luchita0420@gmail.com>
2024-05-09 11:29:08 +0200

    Home Page Responsive Images

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,17 @@ const darkMode = document.querySelector("#mode");
 darkMode.addEventListener("click", () => {
     main.classList.toggle("dark");
 })
+
+// visitor count
+const visits = document.querySelector("#visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits")) || 0;
+
+if (numVisits !== 0) {
+    visits.textContent = numVisits;
+} else {
+    visits.textContent = "This is your first visit!"
+}
+
+numVisits++;
+localStorage.setItem("numVisits", numVisits);
