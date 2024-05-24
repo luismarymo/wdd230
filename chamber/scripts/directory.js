75c2d5f06db1f3c00863a1c41757281050537025
luismarymo <luchita0420@gmail.com>
2024-05-24 19:02:43 +0200

    Chamber Directory Page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,56 @@
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+const directory = document.querySelector("#directory");
+
+const url = "https://luismarymo.github.io/wdd230/chamber/data/members.json";
+
+//get info from json file
+async function getMembers() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            data = await response.json();
+            displayMembers(data.members);
+            
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//add to html
+function displayMembers(members) {
+    members.forEach(member => {
+        //create parent
+        let section = document.createElement("section");
+
+        //populate
+        section.innerHTML = `<img src="${member.logo}" alt="${member.name}">
+        <h3>${member.name}</h3>
+        <address>${member.address}</address>
+        <p>${member.phone}</p>
+        <a href="${member.url}">${member.url}</a>`;
+
+        //append
+        directory.append(section);
+    });
+}
+
+getMembers();
+
+//toggle views
+gridButton.addEventListener("click", () => {
+    directory.classList.add("grid");
+    directory.classList.remove("list");
+    gridButton.classList.add("active");
+    listButton.classList.remove("active");
+})
+
+listButton.addEventListener("click", () => {
+    directory.classList.add("list");
+    directory.classList.remove("grid");
+    listButton.classList.add("active");
+    gridButton.classList.remove("active");
+})
